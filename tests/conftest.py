0abf08caadbabded3240c37750581d2f1669b946
jochen kappel <jkappel@onlinehome.de>
2021-03-10 11:51:01 +0100

    ansible 2.9.18, lifecycle api test added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+import uuid
 
 pytest_plugins = ["docker_compose"]
 
@@ -7,5 +8,9 @@ pytest_plugins = ["docker_compose"]
 @pytest.fixture(scope="module")
 def docker_compose_file(pytestconfig):
    return os.path.join(str(pytestconfig.rootdir), "alm-ansible-rm-docker-compose.yml")
+
+@pytest.fixture
+def get_uuid():
+   return str(uuid.uuid1())
 
 
